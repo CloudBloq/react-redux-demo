9be4fb77ebb69f3ae18bf6a5ec908717b625b098
=yared123yared <=yaredyaya16@gmail.com>
2021-03-20 21:41:22 +0300

    connect  conainer to redux

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {buyCake} from '../redux'
+import {connect} from 'react-redux'
+function CakeContainer (props) {
+    return (
+        <div>
+            <h2>Number of Cake = {props.numOfCake}</h2>
+            <button onClick={props.buyCake}>Buy Cake</button>
+        </div>
+    );
+}
+
+
+const mapStateToProps=state=>{
+    // map the state that came from the redux to the component property
+    return{
+        numOfCake:state.numOfcakes
+    }
+}
+
+const mapDispatchToProps=dispath=>{
+    // map the dispatch came from the redux to the component property
+    return{
+        buyCake:()=>dispath(buyCake())
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CakeContainer);
---
+++ src/redux/cake/cakeReducers.js
@@ -0,0 +1,18 @@
+import {BUY_CAKE} from './cakeType';
+
+const initialState={
+    numOfcakes:10
+}
+
+const cakeReducer=(state=initialState,action)=>{
+    switch(action.type){
+        case BUY_CAKE:
+            return{
+                ...state,
+                numOfcakes:state.numOfcakes -1 
+            }
+            default : return state
+    }
+}
+
+export default cakeReducer;
