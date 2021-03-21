d62c4439c5f15b6c37ca927dc5811c4cfc84a628
=yared123yared <=yaredyaya16@gmail.com>
2021-03-21 09:03:46 +0300

    mapDispatchToProps funcction for both side

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {buyCake,buyIceCream} from '../redux'
 
 function ItemContainer(props){
     return (
         <div>
             <h2>Item - {props.item}</h2>
+            <button onClick={props.buyItem}>Buy Item</button>
         </div>
     );
 }
@@ -15,4 +17,14 @@ const mapStateToProps=(state,ownProps)=>{
     return {item:itemState}
 }
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps=(dispatch,ownProps)=>{
+
+    const  dispathFunction=ownProps.cake ? ()=>dispatch(buyCake(1)) : ()=>dispatch(buyIceCream(1))
+ 
+    return {
+        buyItem:dispathFunction
+    }
+ 
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ItemContainer);
--- src/redux/cake/cakeAction.js
+++ src/redux/cake/cakeAction.js
@@ -1,9 +1,10 @@
 import {BUY_CAKE} from './cakeType'
 
 export const buyCake=(number)=>{
+    const buyNumber=number!=null? number : 1;
     return {
         type:BUY_CAKE,
-        payload:number
+        payload:buyNumber
     }
 }
 
