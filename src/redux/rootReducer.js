319d3ccaa582934a33093a7ee5e1bc031bf7295d
=yared123yared <=yaredyaya16@gmail.com>
2021-03-21 09:41:56 +0300

    Async action

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,11 @@
 import {combineReducers} from 'redux'
 import iceCreamReducer from './iceCream/iceCreamReducers'
 import cakeReducer from './cake/cakeReducers'
-
+import userReducer from './user/userReducer';
 const rootReducer=combineReducers({
     cake:cakeReducer,
-    iceCream:iceCreamReducer
+    iceCream:iceCreamReducer,
+    user:userReducer
 })
 
 export default rootReducer;
